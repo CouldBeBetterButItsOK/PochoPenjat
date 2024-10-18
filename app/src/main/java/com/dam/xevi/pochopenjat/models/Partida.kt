5a95fe120d6b1778cde91b35063f324bf1563a29
CouldBeBetterButItsOk <sanejovex@gmail.com>
2024-10-18 14:49:18 +0200

    Second Commit

--- app/src/main/java/com/dam/xevi/pochopenjat/models/Partida.kt
+++ app/src/main/java/com/dam/xevi/pochopenjat/models/Partida.kt
@@ -2,5 +2,7 @@ package com.dam.xevi.pochopenjat.models
 
 class Partida (
     val jugador1: Jugador,
-    val jugador2: Jugador
+    val jugador2: Jugador,
+    val guanyador: Jugador?,
+    val acabada: Boolean
     )
